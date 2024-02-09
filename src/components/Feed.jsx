31b03a882dc92452f74c8d6f83eeb44e12cc938d
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-02-09 22:47:21 +0530

    Feed component initiated for videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import SideBar from "./SideBar";
+import Videos from "./Videos";
 
 const Feed = () => {
   return (
@@ -20,6 +21,17 @@ const Feed = () => {
         >
           Copyright 2024 ~ D-Alpha Media
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflow: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
